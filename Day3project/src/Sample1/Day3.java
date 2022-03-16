3b3d0cbc5c0343da8e8f8d226797703eea960a7e
Agerneshtesfahun <ageriti@gmail.com>
2022-03-16 11:39:35 -0500

    Commit from Eclipse

---
+++ Day3project/src/Sample1/Day3.java
@@ -0,0 +1,14 @@
+package Sample1;
+
+public class Day3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hellow Miss");
+		System.out.println("Hellow Miss");
+		System.out.println("Hellow Miss");
+
+	}
+
+}
